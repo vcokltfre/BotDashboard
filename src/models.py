9e5f1051c67bc0487358dd12092badc69493d7c6
vcokltfre <vcokltfre@gmail.com>
2021-04-25 17:46:28 +0100

    feat: add authstate model

--- src/models.py
+++ src/models.py
@@ -1,5 +1,13 @@
+from dataclasses import dataclass, field
+
 from pydantic import BaseModel
 
 
 class SaveData(BaseModel):
     code: str
+
+
+@dataclass
+class AuthState:
+    session: str = field(default=None)
+    user: int = field(default=None)
