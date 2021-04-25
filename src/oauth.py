a2868c688c1b0abbaaf778275026b3b1697b4bcd
vcokltfre <vcokltfre@gmail.com>
2021-04-25 15:49:00 +0100

    fix: dont call int on strings

--- src/oauth.py
+++ src/oauth.py
@@ -5,10 +5,10 @@ def build_oauth_token_request(code: str) -> tuple:
     """Given a code, return a dict of query params needed to complete the oath flow."""
     query = dict(
         client_id=int(getenv("CLIENT_ID")),
-        client_secret=int(getenv("CLIENT_SECRET")),
+        client_secret=getenv("CLIENT_SECRET"),
         grant_type="authorization_code",
         code=code,
-        redirect_uri=f"{int(getenv('BASE_URL'))}/callback",
+        redirect_uri=f"{getenv('BASE_URL')}/callback",
         scope="identify",
     )
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
