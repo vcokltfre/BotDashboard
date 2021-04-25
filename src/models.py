05e01ba797c64769c0773c043eee64d1489c091a
vcokltfre <vcokltfre@gmail.com>
2021-04-25 18:00:39 +0100

    feat: add authorize

--- src/models.py
+++ src/models.py
@@ -1,6 +1,8 @@
+from os import getenv
 from dataclasses import dataclass, field
 
 from pydantic import BaseModel
+from fastapi.responses import RedirectResponse
 
 
 class SaveData(BaseModel):
@@ -11,3 +13,8 @@ class SaveData(BaseModel):
 class AuthState:
     session: str = field(default=None)
     user: int = field(default=None)
+
+    def authorize(self, after: str) -> RedirectResponse:
+        redirect = RedirectResponse(getenv("OAUTH_URI"))
+        redirect.set_cookie("oauth_after", after, max_age=60)
+        return redirect
